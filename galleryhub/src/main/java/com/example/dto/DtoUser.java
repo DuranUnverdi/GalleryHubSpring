4a5de62ca2e22d71a89e08e7afdc71a88e40ac87
DuranUnverdi <duranunverdi1905@gmail.com>
2026-04-18 14:36:35 +0300

    Register Fix Response

--- galleryhub/src/main/java/com/example/dto/DtoUser.java
+++ galleryhub/src/main/java/com/example/dto/DtoUser.java
@@ -9,4 +9,28 @@ public class DtoUser extends DtoBase {
     private String username;
     private String password;
 
+    public DtoUser() {
+    }
+
+    public DtoUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
